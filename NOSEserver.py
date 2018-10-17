e32dcf82b0f7da06a8985ffc56fb57b639e0d55c
Andreas Chari <andreas9810@outlook.com>
2018-10-17 09:57:36 +0100

    updated server args

--- NOSEserver.py
+++ NOSEserver.py
@@ -1,7 +1,23 @@
 import socket
+import sys
+import os
 
-client_ip = input('Enter Client IP: ')
-client_port = int(input('Enter Client port: '))
 
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.bind()
+# server IP addr
+server_hostname = socket.gethostname()
+server_ip = socket.gethostbyname(server_hostname)
+print('Server IP: {}'.format(server_ip))
+# server port
+try:
+    listening_port = sys.argv[1]
+except OSError:
+# server full path
+server_path = os.path.dirname(os.path.realpath(__file__)
+print('Server Path: {}'.format(server_path))
+)
+try:
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.bind(listening_port)
+except Exception as exc:
+    print(exc)
+    exit(1)
